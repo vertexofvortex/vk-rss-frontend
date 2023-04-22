b5dc23434436c6174c42fd1615404959ea75e8ad
vertexofvortex <tbm92ru@gmail.com>
2023-04-22 21:10:44 +0300

    🔧 Base URL is now in .env file

--- src/network/axios-instance.ts
+++ src/network/axios-instance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { redirect } from "react-router-dom";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://192.168.0.50:8000",
+  baseURL: import.meta.env.VITE_BASE_URL,
 });
 
 axiosInstance.interceptors.request.use((config) => {
